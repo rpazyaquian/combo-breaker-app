05e678e4a04d9c03555790a777dd959d5d3687af
Rebecca Paz <r.pazyaquian@gmail.com>
2014-12-29 14:11:10 -0500

    managed to import google api key into browserify scripts

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,8 @@
-var gmaps = require('gmaps');
+console.log(process.env.GOOGLE_MAPS_API_KEY);
+
+// var GoogleMapsLoader = require('google-maps');
+
+// GoogleMapsLoader.KEY = process.env.GOOGLE_MAPS_API_KEY;
 
 var address = '177 Linkside Circle';
 var cuisine = 'Chinese';
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 var gulp = require('gulp'),
 connect = require('gulp-connect'),
+uglify = require('gulp-uglify'),
 gutil = require('gulp-util'),
 sourcemaps = require('gulp-sourcemaps'),
 source = require('vinyl-source-stream'),
 buffer = require('vinyl-buffer'),
 watchify = require('watchify'),
-browserify = require('browserify');
+browserify = require('browserify'),
+env = require('node-env-file'),
+envify = require('envify');
+
+env('.env');
+gutil.log(process.env.GOOGLE_MAPS_API_KEY);
 
 var bundler = watchify(browserify('./app/src/main.js', watchify.args));
+
+bundler.transform(envify);
 
 gulp.task('js', bundle);
 bundler.on('update', bundle);
